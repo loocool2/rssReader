05ce1fac993b9b370b52afe3b9197dc9335c7e38
loocool2 <36128021+loocool2@users.noreply.github.com>
2024-05-29 14:57:12 -0700

    initial room db implementation

---
+++ app/src/main/java/com/loocool2/rssreader/local/ArticleEntity.kt
@@ -0,0 +1,16 @@
+package com.loocool2.rssreader.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = DatabaseConstants.ARTICLE_TABLE_NAME)
+data class ArticleEntity(
+    @PrimaryKey(autoGenerate = false)
+    val id: String,
+    val title: String,
+    val link: String,
+    val author: String,
+    val pubDate: Long,
+    val image: String,
+    val feedTitle: String,
+)
---
+++ app/src/main/java/com/loocool2/rssreader/local/DatabaseConstants.kt
@@ -0,0 +1,5 @@
+package com.loocool2.rssreader.local
+
+object DatabaseConstants {
+    const val ARTICLE_TABLE_NAME = "article"
+}
